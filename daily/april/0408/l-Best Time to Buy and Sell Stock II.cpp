0f5340e88d5d838b4c66a03e7a650a8b646ec3a2
kaestro <didme654@snu.ac.kr>
2020-04-26 19:30:17 +0900

    [New library] sListToTree
    transforms singly linked list into Tree.

---
+++ daily/april/0408/l-Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            int diff = prices[i] - prices[i-1];
+            ans += diff > 0 ? diff : 0;
+        }
+        return ans;
+    }
+};
