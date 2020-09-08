ece479c2eea7a217964192167f2062d2d42bf46a
kaestro <didme654@snu.ac.kr>
2020-09-09 05:57:33 +0900

    Maximize the calculation result

--- daily/september/maximize.py
+++ daily/september/maximize.py
@@ -2,32 +2,38 @@ from itertools import permutations
 
 def solution(expression):
     answer = 0
-    operators = ['+', '-', '*']
-    for order in permutations(operators, 3):
-        stack = []
-        for oper in order:
+    operators, numChr = ['+', '-', '*'], [chr(i) for i in range(ord('0'), ord('9') + 1)]
+    numStack, operStack = [], []
+
+    number = None
+    for ch in expression:
+        if ch in numChr:
+            if number is None:
+                number = ch
+            else:
+                number += ch
+        else:
+            numStack.append(int(number))
             number = None
-            for ch in expression:
-                if ch in [chr(i) for i in range(ord('0'), ord('9') + 1)]:
-                    if number is None:
-                        number = ch
-                    else:
-                        number += ch
-                elif ch == oper:
-                    number = int(number)
-                    top = stack.pop()
-                    if oper == "+":
-                        stack.append(top + number)
-                    elif oper == "-":
-                        stack.append(top - number)
-                    else:
-                        stack.append(top * number)
-                    number = None
+            operStack.append(ch)
+    numStack.append(int(number))
+
+
+    for order in permutations(operators, 3):
+        tempNumStack, tempOperStack = numStack[:], operStack[:]
+        for oper in order:
+            operCount = tempOperStack.count(oper)
+            while operCount > 0:
+                idx = tempOperStack.index(oper)
+                tempOperStack.pop(idx)
+                operCount -= 1
+                if oper == "+":
+                    tempNumStack[idx] += tempNumStack.pop(idx + 1)
+                elif oper == "*":
+                    tempNumStack[idx] *= tempNumStack.pop(idx + 1)
                 else:
-                    stack.append(int(number))
-                    stack.append(ch)
-                    number = None
-        answer = max(answer, abs(stack[0]))
+                    tempNumStack[idx] -= tempNumStack.pop(idx + 1)
+        answer = max(answer, abs(tempNumStack[0]))
 
     return answer
 
