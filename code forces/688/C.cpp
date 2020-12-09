6447ef4cf81efab1e8043564dd47c5d6b513825e
kaestro <didme654@snu.ac.kr>
2020-12-09 11:25:09 +0900

    C needs extra case
     The case where 2 points are in line, but will add another one on that
     line
    
     ex)
    
     x x o x x x o x x
     x x x x x x x x x
     x x x x x x x x x
     x x x x x x x x x
     x x o x x x x x x
     x x x x x x x x x
    
     This will need extra point to be added

---
+++ code forces/688/C.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+class Points {
+public:
+    Points(int num, int matSize) {
+        this->num = num;
+        pointsByRow = vvi(matSize);
+        pointsByCol = vvi(matSize);
+        pointsCnt = 0;
+    }
+
+    int num;
+    vvi pointsByRow;
+    vvi pointsByCol;
+    int pointsCnt;
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<string> matrix;
+        vector<Points> vPoints;
+        for (int i = 0; i < 10; ++i) {
+            vPoints.push_back(Points(i, n));
+        }
+
+        matrix.reserve(n);
+        for (int i = 0; i < n; ++i) {
+            string num;
+            cin >> num;
+            matrix.push_back(num);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            string& row = matrix[i];
+            for (int j = 0; j < n; ++j) {
+                int num = row[j] - '0';
+                vPoints[num].pointsCnt++;
+                vPoints[num].pointsByRow[i].push_back(j);
+                vPoints[num].pointsByCol[j].push_back(i);
+            }
+        }
+
+        vector<int> ans;
+        for (auto& points : vPoints) {
+            if (points.pointsCnt < 2) {
+                ans.push_back(0);
+                continue;
+            }
+
+            int maxSize = 0;
+            int num = points.num;
+            for (int i = 0; i < n; ++i) {
+                auto& row = points.pointsByRow[i];
+                if (row.size() == 0) continue;
+
+                int row_len;
+                int col_len;
+                if (row.size() == 1) {
+                    row_len = max(abs(0 - row[0]), n - row[0] - 1);
+                    col_len = 0;
+                    for (int j = 0; j < n; ++j) {
+                        if (points.pointsByRow[j].empty()) continue;
+
+                        col_len = max(col_len, abs(j - i));
+                    }
+                } else {
+                    row_len = row.back() - row.front();
+                    col_len = max(i - 0, n - i - 1);
+                }
+
+                maxSize = max(maxSize, row_len * col_len);
+            }
+
+            for (int i = 0; i < n; ++i) {
+                auto& row = points.pointsByCol[i];
+                if (row.size() == 0) continue;
+
+                int row_len;
+                int col_len;
+                if (row.size() == 1) {
+                    row_len = max(abs(0 - row[0]), n - row[0] - 1);
+                    col_len = 0;
+                    for (int j = 0; j < n; ++j) {
+                        if (points.pointsByCol[j].empty()) continue;
+
+                        col_len = max(col_len, abs(j - i));
+                    }
+                } else {
+                    row_len = row.back() - row.front();
+                    col_len = max(i - 0, n - i - 1);
+                }
+
+                maxSize = max(maxSize, row_len * col_len);
+            }
+
+            ans.push_back(maxSize);
+        }
+        continue;
+    }
+    return 0;
+}
